645ea18ce6e1214114c527babfaad667613c3d24
Liam <urehelonn@gmail.com>
2020-03-26 05:33:59 -0400

    usage of withRouther

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -13,10 +13,8 @@ const HatsPage = () => (
 function App() {
     return (
         <div>
-            <Switch>
-                <Route path="/" component={HomePage}></Route>
-                <Route path="/hats" component={HatsPage}></Route>
-            </Switch>
+            <Route exact path="/" component={HomePage}></Route>
+            <Route path="/hats" component={HatsPage}></Route>
         </div>
     );
 }
