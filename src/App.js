f0d676a59343e636084ad459523a8de5beae6718
Liam <urehelonn@gmail.com>
2020-03-26 06:05:22 -0400

    collection preview page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import PreviewCollection from './pages/preview-collection/preview-collection.component';
 
 const HatsPage = () => (
     <div>
@@ -14,7 +16,8 @@ function App() {
     return (
         <div>
             <Route exact path="/" component={HomePage}></Route>
-            <Route path="/hats" component={HatsPage}></Route>
+            <Route path="/shop" component={ShopPage}></Route>
+            <Route path="/collection-preview" component={PreviewCollection}></Route>
         </div>
     );
 }
